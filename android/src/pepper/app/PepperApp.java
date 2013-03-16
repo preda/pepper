9d6693899ffcabbff14a11b4a41151d51f5ca6b4
Mihai Preda <mhpreda@gmail.com>
2013-03-16 13:26:56 +1100

    fix android build

---
+++ android/src/pepper/app/PepperApp.java
@@ -0,0 +1,22 @@
+// Copyright (C) 2012 - 2013 Mihai Preda
+
+package pepper.app;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+
+public class PepperApp extends Activity {
+    static {
+        System.loadLibrary("pepper");
+    }
+
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(new CanvasView(this));
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
